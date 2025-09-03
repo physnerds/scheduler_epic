cda0f2ea1bffecec685a8131702b006ffc543720
physnerds <amitbashyal@gmail.com>
2025-09-03 16:29:35 -0400

    simreco and ana codes of the drich-mobo workflow modified to work with the scheduler epic

---
+++ tests/pandaidds/drichworkflow/ProjectUtils/ePICUtils/runTestsAndObjectiveCalc_local_sep_analy.py
@@ -0,0 +1,192 @@
+import base64
+import json
+import numpy as np
+import os
+import sys
+import subprocess
+import signal
+import time
+import threading
+# import math
+# import uncertainties
+import logging
+import traceback
+
+from typing import Any
+
+# NEEDS TO:
+# 1. run overlap check
+# 2. generate p/eta scan
+# 3. calculate pi-K sep
+# 4. store all relevant results in drich-mobo-out_{jobid}.txt
+
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s\t%(threadName)s\t%(name)s\t%(levelname)s\t%(message)s')
+
+
+class SubJobManager:
+    def __init__(self, p_eta_point, particle, n_part, job_id, input_name,json_fname=None):
+        self.p_eta_point = p_eta_point
+        self.p_eta_points = [p_eta_point]
+        self.particle = particle
+        self.particles = [particle]
+        self.input_name = input_name
+        self.json_fname = json_fname
+        
+        self.n_part = n_part
+        self.job_id = job_id
+
+        self.dir_path = os.path.dirname(os.path.realpath(__file__))
+        logging.info(f"SubJobManager dir_path: {self.dir_path}")
+        if os.environ.get("AIDE_WORKDIR", None):
+            self.output_dir = os.environ.get("AIDE_WORKDIR")
+        else:
+            self.output_dir = os.getcwd()
+            os.environ['AIDE_WORKDIR'] = self.output_dir
+        self.results_dir = os.path.join(self.output_dir,"log","results")
+        os.makedirs(self.results_dir,exist_ok=True)
+        self.output_name = os.path.join(self.results_dir, "drich-mobo-out_{}.npz".format(job_id))
+        self.status_name = os.path.join(self.results_dir, "drich-mobo-status_{}.txt".format(job_id))
+        for f in [self.output_name, self.status_name]:
+            if os.path.exists(f):
+                os.remove(f)
+
+        # self.particles = ["pi+", "kaon+"]
+        self.final_job_status = {}
+        self.final_job_result = {}
+
+
+    def runJobs(self):
+        logging.info("SubJobManager ++++++ runJobs +++++++")
+        for point_num, p_eta_point in enumerate(self.p_eta_points):
+            # if p_eta_point not in self.final_job_status:
+            #     self.final_job_status[p_eta_point] = {}
+            self.final_job_status[point_num] = {}
+
+            for particle in self.particles:
+                # slurm_file = self.makeSlurmScript(p_eta_point,particle)
+                # shellcommand = ["sbatch",slurm_file]
+
+                p = p_eta_point[0]
+                eta_min = p_eta_point[1][0]
+                eta_max = p_eta_point[1][1]
+                radiator = p_eta_point[2]
+
+                shell_command = [os.path.join(self.dir_path, "shell_wrapper_job_local_analy.sh"),
+                                 str(p), str(eta_min), str(eta_max), str(self.n_part), str(radiator), self.job_id, particle]
+
+                if self.json_fname is not None:
+                    shell_command.append(self.json_fname)
+
+                logging.info("runTest Analy function::runjobs %s",shell_command)
+
+                process = subprocess.run(shell_command)
+
+                status_code = process.returncode
+                logging.info(f"RunTestsAndObjective_local_sep_analy SubJobManager returncode: {status_code}")
+                self.final_job_status[point_num][particle] = int(status_code)
+
+        logging.info("SubJobManager ++++++ end runJobs +++++++")
+        return
+
+    def writeFailedObjectives(self):
+        # executed when we have overlaps and want to punish this result,
+        # but the trial didn't exactly "fail"
+        # TODO: is this how we want to treat this?
+        final_results = np.zeros(6,dtype=float)
+        np.savez(self.output_name, failed_final_results=final_results)
+        return
+
+    def write_status_code(self, status_code):
+        with open(self.status_name, 'w') as f:
+            f.write(str(status_code))
+
+    def retrieveResults(self):
+        # when results finished, retrieve analysis script outputs
+        # and calculate objectives
+        logging.info("SubJobManager retrieving results")
+
+        # results = {}
+        # for i in range(len(self.p_eta_points)):
+        for i, p_eta_point in enumerate(self.p_eta_points):
+            # p_eta_point = self.p_eta_points[i]
+            logging.info(f"SubJobManager job.{i} p_eta_point: {p_eta_point}")
+            p = p_eta_point[0]
+            eta_min = p_eta_point[1][0]
+            eta_max = p_eta_point[1][1]
+            
+            for particle in self.particles:
+                scan_path = os.path.join(os.environ["AIDE_WORKDIR"],
+                                         "log",
+                                         "results",
+                                         #"recon_scan.txt"
+                                    f"recon_scan_{self.job_id}_{particle}_p_{p}_eta_{eta_min}_{eta_max}.txt"
+                                        )
+                if not os.path.exists(scan_path):
+                    logging.error(f"Missing result file {scan_path}")
+                    
+                
+                plus_cher = np.loadtxt(scan_path)
+                key = f"plus_cher_{i}_{particle}_{}"
+                self.final_job_result[key] = np.asarray(plus_cher)
+
+        logging.info(f"SubJobManager saving results to {self.output_name}: {self.final_job_result}")
+        np.savez(self.output_name, **self.final_job_result)
+        logging.info("SubJobManager saved results")
+        return
+
+    def get_final_job_status(self):
+        total, num_done = 0, 0
+        logging.info(f"SubJobManager final_job_status: {self.final_job_status}")
+        for i, p_eta_point in enumerate(self.p_eta_points):
+            for particle in self.particles:
+                total += 1
+                if self.final_job_status[i][particle] == 0:
+                    num_done += 1
+        logging.info(f"SubJobManager final_job_status: num_done {num_done}, total: {total}")
+        return num_done, total
+
+
+def run(jobid, npart, p_eta_point, particle, input_name,json_fname=None):
+
+    manager = SubJobManager(p_eta_point, particle, npart, jobid, input_name, json_fname)
+    
+    manager.runJobs()
+    # manager.monitorJobs()
+
+    manager.retrieveResults()
+    num_done, total = manager.get_final_job_status()
+    if num_done < total:
+        logging.info("some job failed, flag as failure")
+        manager.writeFailedObjectives()
+        manager.write_status_code(1)
+        sys.exit(1)
+
+    else:
+        manager.retrieveResults()
+        logging.info("successfully retrieved results")
+        manager.write_status_code(0)
+        sys.exit(0)
+
+
+if __name__ == "__main__":
+
+    if not(6<= len(sys.argv)<=7):
+        logging.error(f"Number of arguments should be 6 or 7 and not {len(sys.argv)}, exit 1")
+        sys.exit(1)
+
+    logging.info("Running the runTestsAndObjctiveCalc_local_sep_analy.py ")
+    jobid = sys.argv[1]
+    npart = int(sys.argv[2])
+    p_eta_point = sys.argv[3]
+    particle = sys.argv[4]
+    input_name = sys.argv[5]
+    json_fname = sys.argv[6] if len(sys.argv)==7 else None
+
+    try:
+        p_eta_point = json.loads(base64.b64decode(p_eta_point).decode("utf-8"))
+        run(jobid, npart, p_eta_point, particle, input_name,json_fname)
+    except Exception as ex:
+        logging.error(f"RunTestsAndObjectiveCalc_local_sep_analy.py: Failed to run: {ex}")
+        logging.error(traceback.format_exc())
+        sys.exit(1)
---
+++ tests/pandaidds/drichworkflow/ProjectUtils/ePICUtils/runTestsAndObjectiveCalc_local_sep_simreco.py
@@ -0,0 +1,365 @@
+import base64
+import json
+import numpy as np
+import os
+import sys
+import subprocess
+import signal
+import time
+import threading
+# import math
+# import uncertainties
+import logging
+import traceback
+
+from typing import Any
+
+# NEEDS TO:
+# 1. run overlap check
+# 2. generate p/eta scan
+# 3. calculate pi-K sep
+# 4. store all relevant results in drich-mobo-out_{jobid}.txt
+
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s\t%(threadName)s\t%(name)s\t%(levelname)s\t%(message)s')
+
+
+def kill_process_group(pgrp, nap=10):
+    """
+    Kill the process group.
+    DO NOT MOVE TO PROCESSES.PY - will lead to circular import since execute() needs it as well.
+    :param pgrp: process group id (int).
+    :param nap: napping time between kill signals in seconds (int)
+    :return: boolean (True if SIGTERM followed by SIGKILL signalling was successful)
+    """
+
+    status = False
+    _sleep = True
+
+    # kill the process gracefully
+    print(f"killing group process {pgrp}")
+    try:
+        os.killpg(pgrp, signal.SIGTERM)
+    except Exception as error:
+        print(f"exception thrown when killing child group process under SIGTERM: {error}")
+        _sleep = False
+    else:
+        print(f"SIGTERM sent to process group {pgrp}")
+
+    if _sleep:
+        print(f"sleeping {nap} s to allow processes to exit")
+        time.sleep(nap)
+
+    try:
+        os.killpg(pgrp, signal.SIGKILL)
+    except Exception as error:
+        print(f"exception thrown when killing child group process with SIGKILL: {error}")
+    else:
+        print(f"SIGKILL sent to process group {pgrp}")
+        status = True
+
+    return status
+
+
+def kill_all(process: Any) -> str:
+    """
+    Kill all processes after a time-out exception in process.communication().
+
+    :param process: process object
+    :return: stderr (str).
+    """
+
+    stderr = ''
+    try:
+        print('killing lingering subprocess and process group')
+        time.sleep(1)
+        # process.kill()
+        kill_process_group(os.getpgid(process.pid))
+    except ProcessLookupError as exc:
+        stderr += f'\n(kill process group) ProcessLookupError={exc}'
+    except Exception as exc:
+        stderr += f'\n(kill_all 1) exception caught: {exc}'
+    try:
+        print('killing lingering process')
+        time.sleep(1)
+        os.kill(process.pid, signal.SIGTERM)
+        print('sleeping a bit before sending SIGKILL')
+        time.sleep(10)
+        os.kill(process.pid, signal.SIGKILL)
+    except ProcessLookupError as exc:
+        stderr += f'\n(kill process) ProcessLookupError={exc}'
+    except Exception as exc:
+        stderr += f'\n(kill_all 2) exception caught: {exc}'
+    print(f'sent soft kill signals - final stderr: {stderr}')
+    return stderr
+
+
+def run_command_with_timeout(command, timeout=600, stdout=sys.stdout, stderr=sys.stderr):
+    """
+    Run a command and monitor its output. Terminate if no output within timeout.
+    """
+    last_output_time = time.time()
+
+    def monitor_output(stream, output, timeout):
+        nonlocal last_output_time
+        for line in iter(stream.readline, b""):
+            output.buffer.write(line)
+            output.flush()
+            last_output_time = time.time()  # Reset timer on new output
+
+    # Start the process
+    process = subprocess.Popen(command,
+                               preexec_fn=os.setsid,    # setpgrp
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+
+    # Start the monitoring thread
+    stdout_thread = threading.Thread(target=monitor_output, args=(process.stdout, stdout, timeout))
+    stderr_thread = threading.Thread(target=monitor_output, args=(process.stderr, stderr, timeout))
+    stdout_thread.start()
+    stderr_thread.start()
+
+    # monitor the output and enforce timeout
+    while process.poll() is None:
+        time_elapsed = time.time() - last_output_time
+        print(f"time_elapsed: {time_elapsed}, current: {time.time()}, last_update_time: {last_output_time}")
+        if time_elapsed > timeout:
+            print(f"No output for {time_elapsed} seconds. Terminating process.")
+            kill_all(process)
+            break
+        time.sleep(10)  # Check every second
+
+    # Wait for the process to complete and join the monitoring thread
+    stdout_thread.join()
+    stderr_thread.join()
+    process.wait()
+    return process
+
+
+class SubJobManager:
+    def __init__(self, p_eta_point, particle, n_part, job_id, output_root_name):
+        self.p_eta_point = p_eta_point
+        self.p_eta_points = [p_eta_point]
+        self.particle = particle
+        self.particles = [particle]
+
+        # there is only one particle and one p_eta_point,
+        # so the output_root_name can be only one file.
+        self.output_root_name = output_root_name
+
+        self.n_part = n_part
+        self.job_id = job_id
+        # self.outname = str(os.environ["AIDE_HOME"])+"/log/results/"+ "drich-mobo-out_{}.txt".format(jobid)
+        self.dir_path = os.path.dirname(os.path.realpath(__file__))
+        logging.info(f"SubJobManager dir_path: {self.dir_path}")
+        if os.environ.get("AIDE_WORKDIR", None):
+            self.output_dir = os.environ.get("AIDE_WORKDIR")
+        else:
+            self.output_dir = os.getcwd()
+            os.environ['AIDE_WORKDIR'] = self.output_dir
+        self.output_name = os.path.join(self.output_dir, "log/results/drich-mobo-out_{}.npz".format(job_id))
+        self.status_name = os.path.join(self.output_dir, "log/results/drich-mobo-status_{}.txt".format(job_id))
+        for f in [self.output_name, self.status_name]:
+            if os.path.exists(f):
+                os.remove(f)
+
+        # self.particles = ["pi+", "kaon+"]
+        self.final_job_status = {}
+        self.final_job_result = {}
+
+    def checkOverlap(self):
+        #shellcommand = [os.path.join(self.dir_path, "overlap_wrapper_job_local.sh"), str(self.job_id)]
+        shellcommand = [os.path.join(self.dir_path, "overlap_wrapper_job_local.sh"), str(self.job_id)]
+        logging.info("SubJobManager ++++++ checkOverlap +++++++")
+        logging.info(f"SubJobManager command: {shellcommand}")
+        commandout = subprocess.run(shellcommand, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output = commandout.stdout.decode('utf-8')
+        error = commandout.stderr.decode('utf-8')
+        logging.info("SubJobManager output:")
+        logging.info(output)
+        logging.info("SubJobManager error:")
+        logging.info(error)
+        logging.info("SubJobManager ++++++ end checkOverlap +++++++")
+
+        lines = output.split('\n')
+        last_line = lines[-2] if lines else None
+        if last_line:
+            line_split = last_line.split()
+            if len(line_split) == 1:
+                return int(line_split[0])
+            else:
+                return -1
+        else:
+            return -1
+        return -1
+
+    def runJobs(self):
+        logging.info("SubJobManager ++++++ runJobs +++++++")
+        num_jobs = 0
+        for point_num, p_eta_point in enumerate(self.p_eta_points):
+            # if p_eta_point not in self.final_job_status:
+            #     self.final_job_status[p_eta_point] = {}
+            self.final_job_status[point_num] = {}
+
+            for particle in self.particles:
+                # slurm_file = self.makeSlurmScript(p_eta_point,particle)
+                # shellcommand = ["sbatch",slurm_file]
+
+                p = p_eta_point[0]
+                eta_min = p_eta_point[1][0]
+                eta_max = p_eta_point[1][1]
+                radiator = p_eta_point[2]
+
+                #shell_command = [os.path.join(self.dir_path, "shell_wrapper_job_local_simreco.sh"),
+                shell_command = [os.path.join(self.dir_path, "shell_wrapper_job_local_simreco.sh"),
+                                 str(p), str(eta_min), str(eta_max), str(self.n_part), str(radiator), self.job_id, particle, self.output_root_name]
+                logging.info(f"Job No.{num_jobs} SubJobManager command: {shell_command}")
+                # num_jobs += 1
+                # commandout = subprocess.run(shell_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                process = subprocess.run(shell_command)
+                # output = commandout.stdout.decode('utf-8')
+                # error = commandout.stderr.decode('utf-8')
+                # process = run_command_with_timeout(shell_command, timeout=300)
+                status_code = process.returncode
+                logging.info(f"Job No.{num_jobs} SubJobManager returncode: {status_code}")
+                self.final_job_status[point_num][particle] = int(status_code)
+
+                num_jobs += 1
+        logging.info("SubJobManager ++++++ end runJobs +++++++")
+        return
+
+    def writeFailedObjectives(self):
+        # executed when we have overlaps and want to punish this result,
+        # but the trial didn't exactly "fail"
+        # TODO: is this how we want to treat this?
+        final_results = np.array([0, 0, 0, 0, 0, 0])
+        np.savetxt(self.output_name, final_results)
+        return
+
+    def write_status_code(self, status_code):
+        with open(self.status_name, 'w') as f:
+            f.write(str(status_code))
+
+    def retrieveResults(self):
+        # when results finished, retrieve analysis script outputs
+        # and calculate objectives
+        logging.info("SubJobManager retrieving results")
+
+        # results = {}
+        # for i in range(len(self.p_eta_points)):
+        for i, p_eta_point in enumerate(self.p_eta_points):
+            # p_eta_point = self.p_eta_points[i]
+            logging.info(f"SubJobManager job.{i} p_eta_point: {p_eta_point}")
+            p = p_eta_point[0]
+            eta_min = p_eta_point[1][0]
+            eta_max = p_eta_point[1][1]
+
+            for particle in self.particles:
+                plus_cher = np.loadtxt(str(os.environ["AIDE_WORKDIR"]) + "/log/results/" + "recon_scan_{}_{}_p_{}_eta_{}_{}.txt".format(self.job_id, particle, p, eta_min, eta_max))
+                # self.final_job_result[str(p_eta_point)][particle] = plus_cher
+                self.final_job_result[str(i)] = {'point': p_eta_point, 'particle': particle, 'plus_cher': plus_cher.tolist()}
+
+        logging.info(f"SubJobManager saving results to {self.output_name}: {self.final_job_result}")
+        np.savez(self.output_name, **self.final_job_result)
+        logging.info("SubJobManager saved results")
+        return
+
+    def get_final_job_status(self):
+        total, num_done = 0, 0
+        logging.info(f"SubJobManager final_job_status: {self.final_job_status}")
+        for i, p_eta_point in enumerate(self.p_eta_points):
+            for particle in self.particles:
+                total += 1
+                if self.final_job_status[i][particle] == 0:
+                    num_done += 1
+        logging.info(f"SubJobManager final_job_status: num_done {num_done}, total: {total}")
+        return num_done, total
+
+
+def run(jobid, npart, p_eta_point, particle, output_name):
+    # npart = 1500
+    # npart = 2
+    # [momentum, eta range, radiator, dev_piKsep, dev_acc]
+    # std dev for 2500 tracks
+    '''
+    p_eta_scan = [
+        [15, [1.3,2.0], 0, 0.01992527, 0.00700383],
+        [15, [2.0,2.5], 0, 0.01385765, 0.00723669],
+        [15, [2.5,3.5], 0, 0.01577519, 0.00987238],
+        [40, [1.3,2.0], 1, 0.03229172, 0.00645222],
+        [40, [2.0,2.5], 1, 0.01007051, 0.00276879],
+        [40, [2.5,3.5], 1, 0.0106865, 0.00272314]
+    ]
+    '''
+
+    # std dev for 2000 tracks
+    '''
+    p_eta_scan = [
+        [15, [1.5,2.0], 0, 0.02242128, 0.00802887],
+        [15, [2.0,2.5], 0, 0.01539502, 0.00788831],
+        [15, [2.5,3.5], 0, 0.01757623, 0.00998055],
+        [40, [1.5,2.0], 1, 0.03879622, 0.00747648],
+        [40, [2.0,2.5], 1, 0.01182995, 0.00298889],
+        [40, [2.5,3.5], 1, 0.01211408, 0.00309565]
+    ]
+    '''
+
+    '''
+    # std dev for 1500 tracks
+    p_eta_scan = [
+        [15, [1.5, 2.0], 0, 0.02457205, 0.00878185],
+        [15, [2.0, 2.5], 0, 0.01871374, 0.00916126],
+        [15, [2.5, 3.5], 0, 0.02046443, 0.01240257],
+        [40, [1.5, 2.0], 1, 0.04405797, 0.00824205],
+        [40, [2.0, 2.5], 1, 0.01390604, 0.00350744],
+        [40, [2.5, 3.5], 1, 0.01391206, 0.00349814]
+    ]
+    '''
+
+    manager = SubJobManager(p_eta_point, particle, npart, jobid, output_name)
+    noverlaps = manager.checkOverlap()
+    #noverlaps = 1
+    if noverlaps != 0:
+        # OVERLAP OR ERROR, return -1 for all objectives
+        logging.info(f"noverlaps: {noverlaps}, overlaps found, exiting trial")
+        # results = np.array([-1 for i in range(len(p_eta_scan))])
+        # np.savetxt(manager.output_name, results)
+        manager.writeFailedObjectives()
+        sys.exit(0)
+
+    logging.info("no overlaps, starting momentum/eta scan jobs")
+
+    manager.runJobs()
+
+    num_done, total = manager.get_final_job_status()
+    # if np.sum(manager.final_job_status) < 6:
+    if num_done < total:
+        # manager.writeFailedObjectives()
+        manager.write_status_code(1)
+        sys.exit(1)
+        logging.info("some job failed, flag as failure")
+    else:
+        # manager.retrieveResults()
+        logging.info("successfully retrieved results")
+        manager.write_status_code(0)
+        sys.exit(0)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 6:
+        logging.error(f"number of arguments ({len(sys.argv)}) is not 5, exit 1")
+        sys.exit(1)
+
+    jobid = sys.argv[1]
+    npart = int(sys.argv[2])
+    p_eta_point = sys.argv[3]
+    particle = sys.argv[4]
+    output_name = sys.argv[5]
+
+    try:
+        p_eta_point = json.loads(base64.b64decode(p_eta_point).decode("utf-8"))
+        run(jobid, npart, p_eta_point, particle, output_name)
+    except Exception as ex:
+        logging.error(f"failed to run: {ex}")
+        logging.error(traceback.format_exc())
+        sys.exit(1)
